cc749177a76041dd19a8bb9b0cb578bb83df3f36
Michael Chernigin <michaelchernigin@gmail.com>
2023-06-11 10:10:28 +0400

    Test for long expressions

--- tests.py
+++ tests.py
@@ -2,19 +2,27 @@ import subprocess
 import pytest
 
 
-CALC_BIN = './src/calc'
-MODES = ['basic', 'ast_rec', 'ast_iter']
+CALC_BINARY = "./src/calc"
+MODES = ["basic", "ast_rec", "ast_iter"]
 
 
 def run_calc(params):
-    res = subprocess.run([CALC_BIN] + params, stdout=subprocess.PIPE)
+    res = subprocess.run([CALC_BINARY] + params, stdout=subprocess.PIPE)
     assert res.returncode == 0
     return res.stdout.decode().strip()
 
 
-@pytest.mark.parametrize('mode', MODES)
+@pytest.mark.parametrize("mode", MODES)
 def test_simple(mode):
-    expr = '1+2-3*4/5+(7-8)*(9/10)'
+    expr = "1+2-3*4/5+(7-8)*(9/10)"
     res = run_calc(["-p", mode, expr])
-    assert res == '-0.3'
+    assert res == "-0.3"
+
+
+@pytest.mark.parametrize("mode", MODES)
+def test_long(mode):
+    EXPR_LENGTH = 10000
+    expr = "1" + "+1" * EXPR_LENGTH
+    res = run_calc(["-p", mode, expr])
+    assert res == str(1 + EXPR_LENGTH)
 
